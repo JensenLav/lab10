2c7e46593896bb72f08363796151c8cd99c6c5e9
Aria Barbour <arba3926@colorado.edu>
2022-11-11 13:49:18 -0700

    blah

--- project_code_components/index.js
+++ project_code_components/index.js
@@ -67,24 +67,24 @@ const dbConfig = {
 
 
     app.get('/register', (req, res) => {
-        res.render('pages/register',{});
-    });
-        
-    app.post('/register', async (req, res) => {
-
-        
-      const hash = await bcrypt.hash(req.body.password, 10);
-
-      const query = "INSERT INTO users (username, email,  password) VALUES ($1, $2, $3);";
-      db.any(query, [req.body.username, hash])
-      db.any(query, [req.body.email, hash])
-          .then(function (data) {
-              res.redirect("/login");
-          })
-          .catch(function (error) {
-              res.redirect("/register");
-              
-          });
+      res.render('pages/register',{});
+  });
+ 
+  app.post('/register', async (req, res) => {
+        //the logic goes here
+        const hash = await bcrypt.hash(req.body.password, 10);
+    
+        //insert into database
+        let query ="INSERT INTO users(username, password) VALUES($1,$2)";
+        db.any(query, [req.body.username, hash])
+        // db.any(query, [req.body.email, hash])
+        .then(()=> {
+          res.redirect('/login')
+        })
+        .catch(function (err) {
+          console.log(err);
+          res.redirect('/register')
+        });
     });
 
 
@@ -133,16 +133,39 @@ const dbConfig = {
   });
       
   app.post('/postReview', async (req, res) => {
+    //insert into data-base
     const query = "INSERT INTO reviews (rating,  review) VALUES ($1, $2);";
     db.any(query, [req.body.rating, hash])
     db.any(query, [req.body.review, hash])
-        .then(function (data) {
+        .then(() => {
             res.redirect("/home"); //  --------------------in future change to view review page
         })
         .catch(function (error) {
+            console.log(err);
             res.redirect("/postReview");   
         });
   });
+
+//   app.get('/register', (req, res) => {
+//     res.render('pages/register',{});
+// });
+
+// app.post('/register', async (req, res) => {
+//       //the logic goes here
+//       const hash = await bcrypt.hash(req.body.password, 10);
+  
+//       //insert into database
+//       let query ="INSERT INTO users(username, password) VALUES($1,$2)";
+//       db.any(query, [req.body.username, hash])
+//       // db.any(query, [req.body.email, hash])
+//       .then(()=> {
+//         res.redirect('/login')
+//       })
+//       .catch(function (err) {
+//         console.log(err);
+//         res.redirect('/register')
+//       });
+//   });
 
   // viewRestaurants Page
   app.get('/viewRestaurants', (req, res) => {
